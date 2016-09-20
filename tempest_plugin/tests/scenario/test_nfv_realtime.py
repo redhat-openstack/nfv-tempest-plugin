3f2fe7c955609c9dc68d7178295bece5835ed5ec
zshi <zshi@redhat.com>
2016-09-20 15:27:47 +0800

    Added Real Time basic test
    Change-Id: I32a21f018d08bf6bf0f901fc11019ef68e1b820e
    Signed-off-by: zshi <zshi@redhat.com>

---
+++ tempest_plugin/tests/scenario/test_nfv_realtime.py
@@ -0,0 +1,49 @@
+from oslo_log import log
+from tempest import clients
+from tempest import config
+from tempest.common import credentials_factory as common_creds
+from tempest import test
+from tests.scenario import baremetal_manager
+import re
+
+
+CONF = config.CONF
+LOG = log.getLogger(__name__)
+
+HUGEPAGE_SIZE = 1048576
+
+
+class TestRealTimeScenarios(baremetal_manager.BareMetalManager):
+
+    @classmethod
+    def setup_credentials(cls):
+        super(TestRealTimeScenarios, cls).setup_credentials()
+        cls.manager = clients.Manager(
+             credentials=common_creds.get_configured_admin_credentials(
+                fill_in=False))
+
+    def setUp(self):
+        self.image_ref = CONF.compute.image_ref  ## as explained earlier
+        self.flavor_ref = CONF.compute.flavor_ref  ## as explained earlier
+        self.keypairs = {}
+        self.servers = []
+
+        super(TestRealTimeScenarios, self).setUp()
+        self.cpuregex = re.compile('^[0-9]{1,2}$')
+        self.hypervisor_list=dict()
+        self.ip_address = super(TestRealTimeScenarios,self)._get_hypervisor_host_ip('realtime')
+        self.hugepages_init = int(super(TestRealTimeScenarios,
+                                    self)._get_number_free_hugepages(self.ip_address))
+        self.assertNotEqual(self.hugepages_init, 0,
+                                "there is no hugepage"
+                                "on selected realtime node")
+
+    def test_realtime_server(self):
+        LOG.info("TestRealTimeNfvScenarios::test_realtime_server: started" )
+        extra_specs = {'hw:memory_page_size': str(HUGEPAGE_SIZE), 
+                       'hw:cpu_realtime': 'yes', 
+                       'hw:cpu_realtime_mask': '^0',
+                       'hw:cpu_policy': 'dedicated' }
+        flavor_id_rt = self.create_flavor_with_extra_specs(name='rt_flavor', **extra_specs)
+        self.instance = super(TestRealTimeScenarios,self).create_server(image_id=self.image_ref,
+                                          flavor=flavor_id_rt, wait_until='ACTIVE')
