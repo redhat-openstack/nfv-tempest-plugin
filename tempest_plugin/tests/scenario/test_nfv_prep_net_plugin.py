d8d2570a509c1ccf80295f7914237235f32a32f4
Yariv Rachmani <yrachman@redhat.com>
2017-06-15 09:56:37 +0300

    test_nfv_prep_net_plugin.py Add correction for dynamic def gw_ip instead of hardcoded ip Addes tests for multiple network with cloud-init README.md Add detailes for tempest plugin
    baremetal_manager.py
    Modified port creation process and added sec-group
    Modified create-test server to work with empty aggregation-zone
    
    Change-Id: Ie5ebb3490c03f003dbd206c727f87495da87b6ed

--- tempest_plugin/tests/scenario/test_nfv_prep_net_plugin.py
+++ tempest_plugin/tests/scenario/test_nfv_prep_net_plugin.py
@@ -3,7 +3,8 @@ from tempest import config
 from tempest import clients
 from tests.scenario import baremetal_manager
 from tempest.common import credentials_factory as common_creds
-
+import base64
+import textwrap
 
 LOG = logging.getLogger(__name__)
 CONF = config.CONF
@@ -44,13 +45,14 @@ class TestNfvPrepNetPlugin(baremetal_manager.BareMetalManager):
         self.ssh_user = CONF.validation.image_ssh_user
         self.list_networks = []
 
-    def test_server_nfv_network_plugin(self):
+    def test_nfv_provider_network_plugin(self):
         """
         The test shows how to deploy Guest with existing networks..
         external_config.. networks leaf need only network names and port types
         per test setup flavor-id, image-id, availability zone
         The test identifies networks.. create ports preapre port list and parse it to vm
-
+        
+        It relies on l3 provider network, no router
         consume test-setup
         """
         test_setup = 'numa0'
@@ -100,3 +102,80 @@ class TestNfvPrepNetPlugin(baremetal_manager.BareMetalManager):
         """
         msg = "Timed out waiting for %s to become reachable" % ip
         self.assertTrue(self.ping_ip_address(ip), msg)
+
+    def test_tenant_network_cloud_init(self):
+        """
+        The test shows how to deploy Guest with existing networks..
+        external_config.. networks leaf need only network names and port types
+        per test setup flavor-id, image-id, availability zone
+        The test identifies networks.. create ports preapre port list and parse it to vm
+
+        consume test-setup
+        """
+        test_setup = 'numa0'
+        """
+        If the test demands external_config.. apply assertion check for config
+        """
+        self.assertTrue(test_setup in self.test_setup_dict,
+                        "test requires {0}, setup in externs_config_file".
+                        format(test_setup))
+
+        if 'flavor' in self.test_setup_dict[test_setup]:
+            self.flavor_ref = self.test_setup_dict[test_setup]['flavor-id']
+
+        if 'availability-zone' in self.test_setup_dict[test_setup]:
+            self.availability_zone = self.test_setup_dict[test_setup]['availability-zone']
+
+        """
+        Create Keys for Deployed Guest Image
+        """
+        keypair = self.create_keypair()
+        self.key_pairs[keypair['name']] = keypair
+        """
+        Create security groups [icmp,ssh] for Deployed Guest Image
+        """
+        kwargs = {}
+        security_group = self._create_security_group()
+        kwargs['security_groups'] = [{'name': security_group['name'],'id': security_group['id']}]
+
+        super(TestNfvPrepNetPlugin, self)._detect_existing_networks()
+        kwargs['networks'] = super(TestNfvPrepNetPlugin, self).\
+            _create_ports_on_networks(**kwargs)
+
+        gw_ip = self.test_network_dict[self.test_network_dict['public']]['gateway_ip']
+        script = '''
+                 #cloud-config
+                 user: root
+                 password: 12345678
+                 chpasswd: {{expire: False}}
+                 ssh_pwauth: True
+                 disable_root: 0
+                 runcmd:
+                 - cd /etc/sysconfig/network-scripts/
+                 - cp ifcfg-eth0 ifcfg-eth1
+                 - sed -i 's/'eth0'/'eth1'/' ifcfg-eth1
+                 - echo {gateway}{gw_ip} >>  /etc/sysconfig/network
+                 - systemctl restart network'''.format( gateway='GATEWAY=', gw_ip=gw_ip)
+
+        script_clean = textwrap.dedent(script).lstrip().encode('utf8')
+        script_b64 = base64.b64encode(script_clean)
+        kwargs['user_data'] = script_b64
+
+
+        self.instance = self.create_server(key_name=keypair['name'],
+                                           image_id=self.image_ref,
+                                           flavor=self.flavor_ref,
+                                           availability_zone=self.availability_zone,
+                                           wait_until='ACTIVE', **kwargs)
+        """
+        Create floating ip to management port.
+        """
+        fip = self.create_floating_ip(self.instance, self.public_network)
+        LOG.info("fip: %s, instance_id: %s", fip['ip'], self.instance["id"])
+        """
+        Run ping.
+        """
+        msg = "Timed out waiting for %s to become reachable" % fip['ip']
+        self.assertTrue(self.ping_ip_address(fip['ip']), msg)
+
+
