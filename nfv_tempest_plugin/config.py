6acd12ea13a0a321755964d5a68fe717c02547d8
Maxim Babushkin <mbabushk@redhat.com>
2018-08-28 09:22:03 +0300

    Set plugin default paramaters
    Some of the parameters are always the same and don't change.
    Set them as a default in the plugin config.
    Default params could be overridden on the tempest.conf generate from the
    command line.
    
    Change-Id: I7f21d60194b866f0cda6d68c3dbc1779adefbbd1

--- nfv_tempest_plugin/config.py
+++ nfv_tempest_plugin/config.py
@@ -1,4 +1,4 @@
-# Copyright 2015
+# Copyright 2017 Red Hat, Inc.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -12,22 +12,23 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
 from oslo_config import cfg
 
 hypervisor_group = cfg.OptGroup(name="hypervisor",
-                                title="hypervisor params")
+                                title="Hypervisor params")
 
 HypervisorGroup = [
     cfg.StrOpt('user',
-               default='root',
-               help="ssh user to login to bare metal"),
+               default='heat-admin',
+               help="SSH login user"),
     cfg.StrOpt('password',
                default=None,
-               help="ssh password login user"),
+               help="SSH login password"),
     cfg.StrOpt('private_key_file',
-               default=None,
-               help="privatekey string for imported key for ssh user"),
+               default='/home/stack/.ssh/id_rsa',
+               help="Private key string for imported key for ssh user"),
     cfg.StrOpt('external_config_file',
                default=None,
-               help="path to yml file for additional configurations"),
+               help="The path to yml file for additional configurations"),
 ]
