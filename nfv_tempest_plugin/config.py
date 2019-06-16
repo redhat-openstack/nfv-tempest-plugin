962e291dae49400508cbbce0d5d7462f6430eb71
Vadim Khitrin <vkhitrin@redhat.com>
2019-06-16 15:16:12 +0300

    Change transfer method for custom_net_config.py
    Due to a refactor of custom_net_config and the deprecation of
    personality API in future micro versions, changing the transfer nethod
    to cloud-init

--- nfv_tempest_plugin/config.py
+++ nfv_tempest_plugin/config.py
@@ -40,12 +40,6 @@ NfvPluginOptions = [
                help="The path to yml file for additional configurations"),
     cfg.StrOpt('transfer_files',
                default='[{"client_source": "/home/stack/tempest/'
-                       'nfv-tempest-plugin/'
-                       'nfv_tempest_plugin/tests/scenario/tests_scripts/'
-                       'custom_net_config.py",'
-                       '"guest_destination": "/var/lib/cloud/scripts/'
-                       'per-boot/custom_net_config.py"},'
-                       '{"client_source": "/home/stack/tempest/'
                        'nfv-tempest-plugin/nfv_tempest_plugin/tests/scenario/'
                        'tests_scripts/multicast_traffic.py",'
                        '"guest_destination": "/usr/local/bin/'
