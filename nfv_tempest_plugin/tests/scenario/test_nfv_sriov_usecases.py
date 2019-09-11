67797be5d7d79ff06f294b64de73b0efc803488e
Maxim Babushkin <mbabushk@redhat.com>
2019-09-11 23:17:48 +0300

    Add double tagging test
    The double tagging feature allows the vm on the sriov port to use a
    permitted list of vlans.
    The test uses scapy to send ICMP and MPLS traffic between the vms with
    the created vlan based virtual interfaces.
    
    Refer to the following links for more information:
    https://bugs.launchpad.net/neutron/+bug/1693240
    https://bugzilla.redhat.com/show_bug.cgi?id=1497887
    
    Change-Id: Ide809c7f0daab432c11ae97e2c2e4817b3622fb2

--- nfv_tempest_plugin/tests/scenario/test_nfv_sriov_usecases.py
+++ nfv_tempest_plugin/tests/scenario/test_nfv_sriov_usecases.py
@@ -58,3 +58,174 @@ class TestSriovScenarios(base_test.BaseTest):
                     self.assertIn('trust on', line)
         LOG.info('The {} test passed.'.format(test))
         return True
+
+    def test_sriov_double_tagging(self, test='double_tagging', iface_vlan=None,
+                                  test_vlan=None):
+        """Test SRIOV double tagging functionality
+
+        The test require resource creator to setup initial test resources.
+        :param iface_vlan: The vlan that will be set on the virtual interface
+        :param test_vlan: The vlan that will be used for the packets send
+
+        Note! - Both of the vlans should be allowed on the switch ports.
+        """
+        if self.external_resources_data is None:
+            raise ValueError('External resource data is required for the test')
+
+        LOG.info('Start SRIOV double tagging test.')
+        servers, key_pair = self.create_and_verify_resources(test=test)
+        if len(servers) != 4:
+            raise ValueError('The test requires 4 instances.')
+
+        if 'iface_vlan' and 'test_vlan' in \
+                self.test_setup_dict[test]['vlan_config']:
+            iface_vlan = \
+                self.test_setup_dict[test]['vlan_config']['iface_vlan']
+            test_vlan = self.test_setup_dict[test]['vlan_config']['test_vlan']
+            LOG.info('Set test vlans {} and {} for the test'.format(iface_vlan,
+                                                                    test_vlan))
+        else:
+            raise ValueError('Test vlans should be set and allowed in the '
+                             'switch to continue the test')
+
+        trigger = None
+        # Base vlan is the deployment vlan (neutron) and required by PF iface.
+        base_vlan = ''
+        for num, srv in enumerate(servers):
+            srv_ports = self.ports_client.list_ports(
+                device_id=srv['id'])['ports']
+            for port in srv_ports:
+                if port['binding:vnic_type'] in {'direct', 'direct-physical'}:
+                    srv['test_port_type'] = port['binding:vnic_type']
+                    srv['test_mac_addr'] = port['mac_address']
+                    srv['test_ip_addr'] = '10.10.10.1{}'.format(num)
+
+            if srv['test_port_type'] == 'direct-physical':
+                srv['test_name'] = 'pf1'
+            else:
+                srv['test_name'] = 'vf{}'.format(num + 1)
+
+            if trigger is None:
+                cmd = "sudo ip link | awk -F ',' '/{}/ {{print $2}}' " \
+                      "| tr -dc '0-9'".format(srv['test_mac_addr'])
+                base_vlan = self._run_command_over_ssh(
+                    srv['hypervisor_ip'], cmd)
+                trigger = 'stop'
+
+            vf_config = 'sudo python /usr/local/bin/nfv_scripts/double_' \
+                        'tagging_prepare.py --add-iface --port-type ' \
+                        '{port_type} --mac {mac} --vlan {iface_vlan} --addr ' \
+                        '{addr}/24 --install-scapy'
+            pf_config = vf_config + ' --base-vlan {base_vlan}'
+            vf_config = vf_config.format(port_type='vf',
+                                         mac=srv['test_mac_addr'],
+                                         addr=srv['test_ip_addr'],
+                                         iface_vlan=iface_vlan)
+            pf_config = pf_config.format(port_type='pf',
+                                         mac=srv['test_mac_addr'],
+                                         addr=srv['test_ip_addr'],
+                                         iface_vlan=iface_vlan,
+                                         base_vlan=base_vlan)
+            LOG.info('Create and configure virtual interface for '
+                     '{}'.format(srv['test_name']))
+            ssh_source = self.get_remote_client(srv['fip'],
+                                                username=self.instance_user,
+                                                private_key=key_pair[
+                                                    'private_key'])
+            output = ssh_source.exec_command(pf_config if
+                                             srv['test_port_type'] ==
+                                             'direct-physical' else vf_config)
+            srv['test_nic'] = output.split('\n')[-4].split(' ')[-1]
+
+        vf1 = servers[0]
+
+        scapy_sniff = 'sudo python /usr/local/bin/nfv_scripts/scapy_' \
+                      'traffic.py --sniff --keep-sniff -i {iface} -c 5 ' \
+                      '> /dev/null 2>&1 &'
+        scapy_icmp = 'sudo python /usr/local/bin/nfv_scripts/scapy_' \
+                     'traffic.py --{pkt_type} -i {iface} -c 5 --src-mac ' \
+                     '{src_mac} --src-ip {src_ip} --dst-mac {dst_mac} ' \
+                     '--dst-ip {dst_ip} --iface-vlan {iface_vlan} ' \
+                     '--test-vlan {test_vlan}'
+        scapy_mpls = scapy_icmp + ' --raw-msg {raw_msg}'
+
+        send_traffic_cmd = ''
+        LOG.info('Start to sniff for ICMP traffic')
+        for sniffer in servers:
+            if sniffer['test_name'] in ['vf2', 'vf3', 'pf1']:
+                ssh_source = self.get_remote_client(
+                    sniffer['fip'], username=self.instance_user,
+                    private_key=key_pair['private_key'])
+                LOG.info('Start sniffing on {}'.format(sniffer['test_name']))
+                ssh_source.exec_command(scapy_sniff.format(
+                    iface=sniffer['test_nic']))
+                s1 = scapy_icmp.format(pkt_type='icmp', iface=vf1['test_nic'],
+                                       src_mac=vf1['test_mac_addr'],
+                                       src_ip=vf1['test_ip_addr'],
+                                       dst_mac=sniffer['test_mac_addr'],
+                                       dst_ip=sniffer['test_ip_addr'],
+                                       iface_vlan=iface_vlan,
+                                       test_vlan=test_vlan)
+                s2 = scapy_mpls.format(pkt_type='mpls', iface=vf1['test_nic'],
+                                       src_mac=vf1['test_mac_addr'],
+                                       src_ip=vf1['test_ip_addr'],
+                                       dst_mac=sniffer['test_mac_addr'],
+                                       dst_ip=sniffer['test_ip_addr'],
+                                       iface_vlan=iface_vlan,
+                                       test_vlan=test_vlan, raw_msg='testtest')
+                send_traffic_cmd += '{}; {}; '.format(s1, s2)
+        LOG.info('Send ICMP traffic')
+        ssh_source = self.get_remote_client(vf1['fip'],
+                                            username=self.instance_user,
+                                            private_key=key_pair[
+                                                'private_key'])
+        LOG.info('Set scapy packets\n{}'.format(send_traffic_cmd))
+        ssh_source.exec_command(send_traffic_cmd)
+
+        LOG.info('Check for the results')
+        test_results = []
+        for sniffer in servers:
+            if sniffer['test_name'] in ['vf2', 'vf3', 'pf1']:
+                cmd = 'grep output /tmp/scapy_traffic.log | grep -o -e ' \
+                      '"{[^}]*}" | tail -n2 || true'
+                ssh_source = self.get_remote_client(
+                    sniffer['fip'], username=self.instance_user,
+                    private_key=key_pair['private_key'])
+                output = ssh_source.exec_command(cmd)
+                if output:
+                    output = output.splitlines()
+                    for option in output:
+                        opt = eval(option)
+                        if opt['pkt_type'] not in ['icmp', 'mpls']:
+                            test_results.append(
+                                '{} did not catch ICMP or MPLS '
+                                'traffic'.format(sniffer['test_name']))
+                        if opt['pkt_count'] != 5:
+                            test_results.append(
+                                '{} catch less packets than '
+                                'expected'.format(sniffer['test_name']))
+                else:
+                    test_results.append('Output failed for '
+                                        '{}'.format(sniffer['test_name']))
+
+        for srv in servers:
+            vf_remove = 'sudo python /usr/local/bin/nfv_scripts/double_' \
+                        'tagging_prepare.py --del-iface --port-type vf ' \
+                        '--mac {mac} --vlan {iface_vlan}'\
+                .format(mac=srv['test_mac_addr'], iface_vlan=iface_vlan)
+            pf_remove = 'sudo python /usr/local/bin/nfv_scripts/double_' \
+                        'tagging_prepare.py --del-iface --port-type pf ' \
+                        '--mac {mac} --base-vlan {base_vlan}'\
+                .format(mac=srv['test_mac_addr'], base_vlan=base_vlan)
+            remove_log_file = '; sudo rm -f /tmp/scapy_traffic.log'
+            vf_remove += remove_log_file
+            pf_remove += remove_log_file
+            ssh_source = self.get_remote_client(srv['fip'],
+                                                username=self.instance_user,
+                                                private_key=key_pair[
+                                                    'private_key'])
+            ssh_source.exec_command(pf_remove if
+                                    srv['test_port_type'] ==
+                                    'direct-physical' else vf_remove)
+
+        self.assertEmpty(test_results, test_results)
