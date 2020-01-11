3afe4a3670ae340f27ba33d9c1b96bb03c95031d
Vadim Khitrin <me@vkhitrin.com>
2020-01-11 02:19:00 +0200

    test_all_provider_networks: Verify guest provider networks
    Add an option to plugin to verify all provider networks attached to
    instance.
    
    The verification will be performed during instance creation.
    Verification includes testing guest instance interface configuration and
    icmp ping between multiple servers when available.

--- nfv_tempest_plugin/tests/scenario/base_test.py
+++ nfv_tempest_plugin/tests/scenario/base_test.py
@@ -46,6 +46,46 @@ class BaseTest(baremetal_manager.BareMetalManager):
         super(BaseTest, self).setUp()
         # pre setup creations and checks read from config files
 
+    def verify_provider_networks(self, servers=None, key_pair=None):
+        """Pings from all guest provider networks to verify connectivity
+
+        This functions attempts to verify all provider networks present
+        inside guest.
+
+        If multiple servers are created, guests will attempt to ping between
+        themselves on each provider network.
+
+        :param servers: List of servers created
+        :param key-pair: Key pair used to authenticate with server
+        """
+        for server in servers:
+            # Save index location of current server in servers
+            server_index = servers.index(server)
+            # Remove current server object from servers
+            servers.remove(server)
+            # Initialize a custom key inside server object
+            server['provider_networks'] = []
+            # Fetch all ports assigned to server
+            ports =  \
+                self.os_admin.ports_client.list_ports(device_id=server['id'])
+            for port in ports['ports']:
+                provider_dict = {
+                    'mac_address': port['mac_address'],
+                    'ip_address': port['fixed_ips'][0]['ip_address']
+                }
+                server['provider_networks'].append(provider_dict)
+            # Insert updated server object to servers
+            servers.insert(server_index, server)
+            # Create an SSH connection to server
+            ssh_client = self.get_remote_client(server['fip'],
+                                                self.instance_user,
+                                                key_pair['private_key'])
+            self.check_guest_interface_config(ssh_client,
+                                              server['provider_networks'],
+                                              server['name'])
+
+        self.check_guest_provider_networks(servers, key_pair)
+
     def create_and_verify_resources(self, test=None, fip=None, **kwargs):
         """Create and verify resources method
 
@@ -91,5 +131,9 @@ class BaseTest(baremetal_manager.BareMetalManager):
                 ping = self.ping_via_network_namespace(srv['fip'],
                                                        srv['network_id'])
                 self.assertTrue(ping)
+
+        # Verify provider networks only when requested and if FIP is assigned
+        if self.test_all_provider_networks and fip:
+            self.verify_provider_networks(servers, key_pair)
 
         return servers, key_pair
