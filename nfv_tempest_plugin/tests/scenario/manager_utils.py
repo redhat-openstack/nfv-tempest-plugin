7ef8d0148a6fe95ea4b45e00856dc98bda183aaf
Vadim Khitrin <me@vkhitrin.com>
2020-11-30 17:07:12 +0200

    igmp: Add OVN support to IGMP use cases
    * Fix network backend detection function: dicover_deployment_network_backend -> discover_deployment_network_backend
    * Support test_igmp_snooping_deployment in OVN deployment
    * Support test_igmp_snooping in OVN deployment
    * Support test_igmp_restart_ovs in OVN deployment (should be renamed in the future)
    
    Change-Id: I7171b8a9cdd157511fc15c0261c79bcd7a07ec57

--- nfv_tempest_plugin/tests/scenario/manager_utils.py
+++ nfv_tempest_plugin/tests/scenario/manager_utils.py
@@ -793,6 +793,32 @@ class ManagerMixin(object):
                     output_data.append(data)
         return output_data
 
+    def get_ovn_multicast_groups(self):
+        """Retrieves OVN multicast groups from OVN southbound DB
+
+        :return multicast groups
+        """
+        output_data = []
+        multicast_ips = []
+        controller = shell_utils.get_controllers_ip_from_undercloud(
+            shell=CONF.nfv_plugin_options.undercloud_rc_file)[0]
+        ovn_logical_switches_cmd = ('sudo podman exec -it ovn_controller'
+                                    ' ovn-sbctl list igmp_group')
+        ovn_igmp_output = shell_utils.run_command_over_ssh(
+            controller, ovn_logical_switches_cmd)
+        for string in re.split(r'\n+', ovn_igmp_output):
+            if 'address' in string:
+                igmp_ip = re.sub(r'address\s+ :\s+',
+                                 '', string.rstrip())
+                multicast_ips.append(igmp_ip.replace('"', ''))
+        if multicast_ips:
+            # Iterate over unique IP entries
+            for ip in set(multicast_ips):
+                data = {}
+                data['GROUP'] = ip
+                output_data.append(data)
+        return output_data
+
     def _get_hypervisor_host_ip(self, name=None):
         """Get hypervisor ip
 
@@ -898,7 +924,7 @@ class ManagerMixin(object):
             hyper_kwargs = {'shell':
                             CONF.nfv_plugin_options.undercloud_rc_file}
             node = self._get_hypervisor_ip_from_undercloud(**hyper_kwargs)[0]
-        network_backend = self.dicover_deployment_network_backend(node=node)
+        network_backend = self.discover_deployment_network_backend(node=node)
         if not keys:
             if network_backend == 'ovs':
                 hiera_bridge_mapping = \
@@ -1020,7 +1046,7 @@ class ManagerMixin(object):
                 'vcpu_free_per_numa': vcpu_free_per_numa,
                 'ram_free': ram_free}
 
-    def dicover_deployment_network_backend(self, node=None):
+    def discover_deployment_network_backend(self, node=None):
         """Locate deployment's network backend
 
         The method discovers the network backend used in deployment.
