deb3caee9d2b8bd4edf3835982c7d16af4b24ffb
Maxim Babushkin <mbabushk@redhat.com>
2021-02-01 10:43:52 +0200

    Update hypervisor_tuning test
    - Fix test name typo
    - Packages test stage takes the list of required packages and grep them
      from the hypervisor. Then compares amount of the packages between the
      required list and hypervisor results.
      In order to ensure that every required packages exist in the output
      result, using fnmatch to ensure it.
    - Removing the "tuned.non_isolcpus" kernel arg from the kernel_args
      list. The real time deployment has a different profile and the
      "tuned.non_isolcpus" param does not exists over there.
      In order to not complicate the test logic, removing the argument.
      Other arguments will remain as required and exist in both deployment
      types.
    
    Change-Id: I774fff9b4083c1517939baf7eee6d2396eedb929

--- nfv_tempest_plugin/tests/scenario/test_nfv_basic.py
+++ nfv_tempest_plugin/tests/scenario/test_nfv_basic.py
@@ -14,6 +14,7 @@
 #    under the License.
 
 import json
+import fnmatch
 
 from nfv_tempest_plugin.tests.common import shell_utilities as shell_utils
 from nfv_tempest_plugin.tests.scenario import base_test
@@ -38,7 +39,7 @@ class TestNfvBasic(base_test.BaseTest):
         super(TestNfvBasic, self).setUp()
         # pre setup creations and checks read from config files
 
-    def test_hypevisor_tuning(self):
+    def test_hypervisor_tuning(self):
         """Test tuning state of hypervisor
 
         Test the following states:
@@ -68,11 +69,12 @@ class TestNfvBasic(base_test.BaseTest):
             result = shell_utils.run_command_over_ssh(self.hypervisor_ip,
                                                       pkg_check).split()
             if result:
-                if len(result) != len(packages):
-                    test_result.append("Missing required packages. "
-                                       "Found following packages: {}"
-                                       .format(result))
-                    LOG.info("Found the following packages: {}".format(result))
+                for pkg in packages:
+                    if not fnmatch.filter(result, pkg):
+                        test_result.append("Missing required packages. "
+                                           "Found following packages: {}"
+                                           .format(result))
+                LOG.info("Found the following packages: {}".format(result))
             else:
                 test_result.append("Packages: no output received")
 
