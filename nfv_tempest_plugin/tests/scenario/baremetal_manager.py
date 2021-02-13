a9861481850a4dfb1a9a5fcb4ada4f1fdb1eb5c9
Miguel Angel Nieto Jimenez <mnietoji@redhat.com>
2021-02-13 00:35:25 +0000

    Add retries to ping to provider networks
    It is used the function ping_host from
    tempest/lib/common/utils/linux/remote_client.py which executes
    ping -c1 -w1 -s56 10.10.10.103. Sometimes the deadline is very
    short and the ping fails. It is possible to increase it through the
    tempest configuration file(verification.ping_count) together with
    the number of pings, but it fails the first ping and though the next
    ping are received, it returns an error
    This issue is causing to fail testcases test_sriov_free_resource and
    test_igmp_snooping
    
    Change-Id: I4e2716b1ef72c19c84867ea53d201c99c7d8d5f2

--- nfv_tempest_plugin/tests/scenario/baremetal_manager.py
+++ nfv_tempest_plugin/tests/scenario/baremetal_manager.py
@@ -1017,13 +1017,15 @@ class BareMetalManager(api_version_utils.BaseMicroversionTest,
             for neighbors_ip in neighbors_ips:
                 LOG.info("Guest '{h}' will attempt to "
                          "ping {i}".format(h=hostname, i=neighbors_ip))
-                try:
-                    ssh_client.icmp_check(neighbors_ip)
-                except lib_exc.SSHExecCommandFailed:
-                    msg = ("Guest '{h}' failed to ping "
-                           "IP '{i}'".format(h=hostname, i=neighbors_ip))
-                    raise AssertionError(msg)
-
+                max_num_tries = 3
+                for ping_try in range(max_num_tries):
+                    try:
+                        ssh_client.icmp_check(neighbors_ip)
+                    except lib_exc.SSHExecCommandFailed:
+                        if ping_try >= (max_num_tries - 1):
+                            msg = ("Guest '{h}' failed to ping "
+                                   "IP '{i}'".format(h=hostname, i=neighbors_ip))
+                            raise AssertionError(msg)
                 LOG.info("Guest '{h}' successfully was able to ping "
                          "IP '{i}'".format(h=hostname, i=neighbors_ip))
 
