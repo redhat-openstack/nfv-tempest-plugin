ea1f4db09c65a530534cc1c59b9670ccd6186855
Ella Shulman <eshulman@redhat.com>
2021-03-10 15:31:06 +0200

    [RFE] tests compute doesn't reboot meanwile update
    - added a multi thread wrapper for functions
    - created tests mentioned in the following test plan:
      https://docs.google.com/document/d/1ZDiobclzyBC7EuY7pwORJv2vfKWFpmbBHqnMCOP7Z9w/edit?usp=sharing
    - moved reboot and wait for node into a seperate function
    - created services classes that initiate clients to both under cloud and overcloud
    
    Change-Id: I3edd8607f1add07c0648821baf8ca2b35952bd2f

---
+++ nfv_tempest_plugin/services/nova_client.py
@@ -0,0 +1,31 @@
+
+from nfv_tempest_plugin.services.keystone_client import KeystoneClient
+from novaclient.client import Client
+from tempest import config
+
+CONF = config.CONF
+
+
+class NovaClient(KeystoneClient):
+    @classmethod
+    def set_nova_clients(cls):
+        super().set_keystone_clients()
+
+        cls.novaclient_overcloud = Client(version=CONF
+                                          .compute.max_microversion,
+                                          session=cls
+                                          .overcloud_keystone_session)
+        cls.novaclient_undercloud = Client(version=CONF
+                                           .compute.max_microversion,
+                                           session=cls
+                                           .undercloud_keystone_session)
+
+    def overcloud_hypervisor_to_undecloud_server(self, hypervisor):
+        """take in hypervisor object and find its related undercloud server
+
+        :param hypervisor: nova client hypervisor object
+        :return nova client server object
+        """
+        hypervisor_name = hypervisor.hypervisor_hostname.split('.')[0]
+        return self.novaclient_undercloud.servers.list(search_opts={
+            'hostname': hypervisor_name})[0]
