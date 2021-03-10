e5df336681e2b4a511b53e2607dd654739c0639c
Ella Shulman <eshulman@redhat.com>
2021-03-10 15:31:06 +0200

    [RFE] tests compute doesn't reboot meanwile update
    - added a multi thread wrapper for functions
    - created tests mentioned in the following test plan:
      https://docs.google.com/document/d/1ZDiobclzyBC7EuY7pwORJv2vfKWFpmbBHqnMCOP7Z9w/edit?usp=sharing
    - moved reboot and wait for node into a seperate function
    - created services classes that initiate clients to both under cloud and overcloud
    
    Change-Id: I3edd8607f1add07c0648821baf8ca2b35952bd2f

---
+++ nfv_tempest_plugin/services/keystone_client.py
@@ -0,0 +1,35 @@
+
+from envbash import load_envbash
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from os import environ
+from oslo_log import log as logging
+from tempest import config
+
+
+CONF = config.CONF
+LOG = logging.getLogger('{} [-] nfv_plugin_test'.format(__name__))
+
+
+class KeystoneClient():
+    def __init__(self):
+        load_envbash(
+            CONF.nfv_plugin_options.undercloud_rc_file)
+
+        self.undercloud_keystone_session = session.Session(auth=v3.Password(
+            auth_url=environ['OS_AUTH_URL'],
+            username=environ['OS_USERNAME'],
+            password=environ['OS_PASSWORD'],
+            project_name=environ['OS_PROJECT_NAME'],
+            user_domain_name=environ[
+                'OS_USER_DOMAIN_NAME'],
+            project_domain_name=environ[
+                'OS_PROJECT_DOMAIN_NAME']), verify=False)
+
+        self.overcloud_keystone_session = session.Session(auth=v3.Password(
+            auth_url=CONF.identity.uri,
+            username=CONF.auth.admin_username,
+            password=CONF.auth.admin_password,
+            project_name=CONF.auth.admin_project_name,
+            user_domain_name=CONF.auth.admin_domain_name,
+            project_domain_name=CONF.auth.admin_domain_name), verify=False)
---
+++ nfv_tempest_plugin/services/nova_client.py
@@ -0,0 +1,30 @@
+
+from nfv_tempest_plugin.services.keystone_client import KeystoneClient
+from novaclient.client import Client
+from tempest import config
+
+CONF = config.CONF
+
+
+class NovaClient(KeystoneClient):
+    def __init__(self):
+        super(NovaClient, self).__init__()
+
+        self.novaclient_overcloud = Client(version=CONF
+                                           .compute.max_microversion,
+                                           session=self
+                                           .overcloud_keystone_session)
+        self.novaclient_undercloud = Client(version=CONF
+                                            .compute.max_microversion,
+                                            session=self
+                                            .undercloud_keystone_session)
+
+    def overcloud_hypervisor_to_undecloud_server(self, hypervisor):
+        """take in hypervisor object and find its related undercloud server
+
+        :param hypervisor: nova client hypervisor object
+        :return nova client server object
+        """
+        hypervisor_name = hypervisor.hypervisor_hostname.split('.')[0]
+        return self.novaclient_undercloud.servers.list(search_opts={
+            'hostname': hypervisor_name})[0]
