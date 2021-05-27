59ee1318f4209418a7980d3387e1066b6c59df79
Ella Shulman <eshulman@redhat.com>
2021-05-27 10:36:25 +0300

    [RFE] tests compute doesn't reboot meanwile update
    - added a multi thread wrapper for functions
    - created tests mentioned in the following test plan:
      https://docs.google.com/document/d/1ZDiobclzyBC7EuY7pwORJv2vfKWFpmbBHqnMCOP7Z9w/edit?usp=sharing
    - moved reboot and wait for node into a seperate function
    - created services classes that initiate clients to both under cloud and overcloud
    - with python 2.7 backword competability
    
    Change-Id: I90286ec5e8ab6ce572a25d6d70768a8353376e37

---
+++ nfv_tempest_plugin/services/heat_client.py
@@ -0,0 +1,19 @@
+
+from heatclient.client import Client
+from nfv_tempest_plugin.services.keystone_client import KeystoneClient
+from tempest import config
+
+CONF = config.CONF
+
+
+class HeatClient(KeystoneClient):
+    def __init__(self):
+        super().__init__()
+
+        self.undercloud_heatclient = Client('1',
+                                            session=self
+                                            .undercloud_keystone_session)
+
+        self.overcloud_heatclient = Client('1',
+                                           session=self
+                                           .overcloud_keystone_session)
---
+++ nfv_tempest_plugin/services/os_clients.py
@@ -0,0 +1,8 @@
+
+from nfv_tempest_plugin.services.heat_client import HeatClient
+from nfv_tempest_plugin.services.nova_client import NovaClient
+
+
+class OsClients(HeatClient, NovaClient):
+    def __init__(self):
+        super().__init__()
