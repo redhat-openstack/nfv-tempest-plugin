d33f9f53d093f46d564414880d8c8317faadfa85
Ella Shulman <eshulman@redhat.com>
2021-05-31 16:32:05 +0300

    fixed _get_hypervisor_ip_from_undercloud
    - changed _get_hypervisor_ip_from_undercloud to work with the api and
    return ip instead of name
    - removed not used option
    
    Change-Id: Icb166be8bf47e5376829e168c12c609678a96b08

--- nfv_tempest_plugin/tests/scenario/base_test.py
+++ nfv_tempest_plugin/tests/scenario/base_test.py
@@ -145,8 +145,7 @@ class BaseTest(baremetal_manager.BareMetalManager):
                 srv['fip'], srv['id']))
 
             srv['hypervisor_ip'] = self._get_hypervisor_ip_from_undercloud(
-                **{'shell': CONF.nfv_plugin_options.undercloud_rc_file,
-                   'server_id': srv['id']})[0]
+                **{'server_id': srv['id']})[0]
             self.assertNotEmpty(srv['hypervisor_ip'],
                                 "_get_hypervisor_ip_from_undercloud "
                                 "returned empty ip list")
