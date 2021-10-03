188afb2de6fe1e56e20317b8d635c961edeb1a65
Yarboa <yrachman@redhat.com>
2021-10-03 13:44:05 +0300

    [microversion 2.72]
    This patch include fixes for running with 2.72 microversion
    * Currently Live migration is not working with 2.72, but code is ready
    * Adding here new manager clients for max/min qos rules,
      This change is related to clients refactoring in neutron-tempest-plugin
    
    Change-Id: I6659fe367595f9d468265990c39244742a9ac852

--- nfv_tempest_plugin/services/network_client_v2.py
+++ nfv_tempest_plugin/services/network_client_v2.py
@@ -1,6 +1,8 @@
 from neutron_tempest_plugin.api import clients as manager
 from neutron_tempest_plugin.services.network.json import network_client
 from tempest import config
+from tempest.lib.services.network import qos_limit_bandwidth_rules_client
+from tempest.lib.services.network import qos_minimum_bandwidth_rules_client
 
 CONF = config.CONF
 
@@ -16,3 +18,23 @@ class Manager(manager.Manager):
             build_interval=CONF.network.build_interval,
             build_timeout=CONF.network.build_timeout,
             **self.default_params)
+
+        self.qos_limit_bandwidth_rules_client = \
+            qos_limit_bandwidth_rules_client.QosLimitBandwidthRulesClient(
+                self.auth_provider,
+                CONF.network.catalog_type,
+                CONF.network.region or CONF.identity.region,
+                endpoint_type=CONF.network.endpoint_type,
+                build_interval=CONF.network.build_interval,
+                build_timeout=CONF.network.build_timeout,
+                **self.default_params)
+
+        self.qos_minimum_bandwidth_rules_client = \
+            qos_minimum_bandwidth_rules_client.QosMinimumBandwidthRulesClient(
+                self.auth_provider,
+                CONF.network.catalog_type,
+                CONF.network.region or CONF.identity.region,
+                endpoint_type=CONF.network.endpoint_type,
+                build_interval=CONF.network.build_interval,
+                build_timeout=CONF.network.build_timeout,
+                **self.default_params)
