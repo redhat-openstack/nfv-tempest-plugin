f6e2e4c6b5a7415f693b12bf6d920c792fbc601d
eshulman <eshulman@redhat.com>
2022-05-25 19:01:23 +0300

    - added metalsmith client and fixed changed keystone client to allow openstack connection - fixed _get_hypervisor_ip_from_undercloud function
    Change-Id: I0d0d4b1198d16a34133e7fbffd44c67899344b24

--- nfv_tempest_plugin/tests/common/collect_info.py
+++ nfv_tempest_plugin/tests/common/collect_info.py
@@ -1,13 +1,15 @@
 from oslo_log import log as logging
-import re
+import subprocess
 
 LOG = logging.getLogger('{} [-] nfv_plugin_test'.format(__name__))
 
 
 class CollectInfo():
-    def get_osp_version():
-        version_pattern = re.compile(r'\d+.\d+.\d+')
-        with open('/etc/rhosp-release', 'r') as release:
-            version = version_pattern.search(release.read())
+    def check_client():
+        metalsmith_show = "source ~/stackrc && metalsmith list"
+        try:
+            subprocess.check_output(['bash', '-c', metalsmith_show])
+            return "metalsmith"
+        except subprocess.CalledProcessError:
+            return "nova"
 
-        return version.group(0)
