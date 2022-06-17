3d12d7d585e27e75142540026c9f1b984d89b530
Ricardo Diaz <rdiazcam@redhat.com>
2022-06-17 16:16:01 +0200

    Fix check client nova/metalsmith
    The built-in bash source command is not properly handled by python subprocess.
    Use --rcfile as argument instead.
    
    Change-Id: I3db145194ee614a56805a5ff739bd4c0d4feb47e

--- nfv_tempest_plugin/tests/common/collect_info.py
+++ nfv_tempest_plugin/tests/common/collect_info.py
@@ -7,9 +7,10 @@ LOG = logging.getLogger('{} [-] nfv_plugin_test'.format(__name__))
 class CollectInfo():
     def check_client():
         """Return the undercloud show supported undercloud server api"""
-        nova_show = "source ~/stackrc && nova list"
+        nova_show = "nova list"
         try:
-            subprocess.check_output(['bash', '-c', nova_show])
+            subprocess.check_output(['bash', '--rcfile', '~/stackrc', '-c',
+                                     nova_show])
             return "nova"
         except subprocess.CalledProcessError:
             return "metalsmith"
