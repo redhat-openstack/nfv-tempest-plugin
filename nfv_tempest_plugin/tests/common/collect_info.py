6331c53feb5548598d5fab731cd3e1b4cdd9bd08
Ricardo Diaz <rdiazcam@redhat.com>
2022-06-21 16:23:35 +0200

    Return UC client based on rhosp-release
    Decide the UC client based on the output of 'nova/metalsmith list' can be error
    prone because that command needs to load runtime configuration files and that
    can be a problem when running local commands with already defined environment
    variables.
    
    Change-Id: I6c3c20ae27d97fb827bbfb1b42d578ce6236b120

--- nfv_tempest_plugin/tests/common/collect_info.py
+++ nfv_tempest_plugin/tests/common/collect_info.py
@@ -7,10 +7,9 @@ LOG = logging.getLogger('{} [-] nfv_plugin_test'.format(__name__))
 class CollectInfo():
     def check_client():
         """Return the undercloud show supported undercloud server api"""
-        nova_show = "nova list"
-        try:
-            subprocess.check_output(['bash', '--rcfile', '~/stackrc', '-c',
-                                     nova_show])
-            return "nova"
-        except subprocess.CalledProcessError:
-            return "metalsmith"
+
+        output = subprocess.check_output(['cat', '/etc/rhosp-release'])
+        rhosp_version = int(re.findall(b'\d+', output)[0])
+        if rhosp_version >= 17:
+            return 'metalsmith'
+        return 'nova'
