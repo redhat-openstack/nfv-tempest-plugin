1cee14b7c956e74ea45c3fef60803c9f58718209
Vadim Khitrin <me@vkhitrin.com>
2022-09-16 20:48:33 +0300

    test: Update Tuning Test To Support AMD/INTEL CPUs
    Removing a hard coded kernel argument that assumes that all deployment
    contain Intel CPU.
    test_hypervisor_tuning will derive the appropriate IOMMU kernel argument
    based on the CPU manufacturer.
    
    Change-Id: I6415fd78a4095d09260a20d5391c6be76b79ad4a

--- nfv_tempest_plugin/config.py
+++ nfv_tempest_plugin/config.py
@@ -116,7 +116,7 @@ NfvPluginOptions = [
                        '"openvswitch"], "tuned_profiles": ["cpu-partitioning",'
                        '"realtime-virtual-host"], "kernel_args": ["hugepages",'
                        '"hugepagesz", "default_hugepagesz", "iommu=pt",'
-                       '"intel_iommu=on", "isolcpus", "nohz=on", "nohz_full",'
+                       '"isolcpus", "nohz=on", "nohz_full",'
                        '"rcu_nocbs", "intel_pstate"]}',
                help='Hypervisor tuning details include: packages, '
                     'services, tuned profiles nad kernel arguments'),
