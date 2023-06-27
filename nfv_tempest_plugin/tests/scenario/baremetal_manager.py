2dd099b0eb81cc616022117b9d8b348c00cf3937
Miguel Angel Nieto Jimenez <mnietoji@redhat.com>
2023-06-27 18:53:30 +0200

    Remove security group rules
    Change-Id: I1e8254bfcba6627f0b0acfbe11b15c8e74bbb25c

--- nfv_tempest_plugin/tests/scenario/baremetal_manager.py
+++ nfv_tempest_plugin/tests/scenario/baremetal_manager.py
@@ -1292,10 +1292,14 @@ class BareMetalManager(api_version_utils.BaseMicroversionTest,
 
         for rule in rule_list:
             direction = rule.pop('direction')
-            client_rules.create_security_group_rule(
+            secgrouprule = client_rules.create_security_group_rule(
                 direction=direction,
                 security_group_id=secgroup_id,
                 **rule)
+            self.addCleanup(
+                test_utils.call_and_ignore_notfound_exc,
+                self.security_groups_client.delete_security_group_rule,
+                secgrouprule['id'])
 
     def get_security_group_from_partial_string(self, group_name_string):
         """Get security group based on partial string
