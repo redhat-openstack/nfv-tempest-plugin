3d5d78b392b6af871a794cec52ab8bffea4472e5
Sanjay Upadhyay <supadhya@redhat.com>
2023-09-05 12:56:57 +0530

    (WIP) fix test_mq_autob_no_improvement
    Change-Id: Id96ba4ecd6126ed583de01dea3a79ed075e48e34

--- nfv_tempest_plugin/tests/scenario/test_multiqueue_usecases.py
+++ nfv_tempest_plugin/tests/scenario/test_multiqueue_usecases.py
@@ -161,11 +161,46 @@ class TestMultiqueueScenarios(base_test.BaseTest):
         # Get trex and testpmd vms
         servers_dict = self.prepare_vms(test)
 
+        cmd_load_improvement_threshold = 'sudo ovs-vsctl --no-wait ' \
+            'set open_vSwitch . other_config:pmd-auto-lb-improvement' \
+            '-threshold="50"'
+        cmd_load_rebal_interval = 'sudo ovs-vsctl --no-wait set ' \
+            'open_vSwitch . other_config:pmd-auto-lb-rebal-interval=' \
+            '"10"'
+
+        LOG.info('Reset load_threshold cmd {}'.
+                 format(cmd_load_improvement_threshold))
+        shell_utils.run_command_over_ssh(
+            servers_dict['testpmd']['hypervisor_ip'],
+            cmd_load_improvement_threshold)
+        LOG.info('Reset load_threshold cmd {}'.
+                 format(cmd_load_rebal_interval))
+        shell_utils.run_command_over_ssh(
+            servers_dict['testpmd']['hypervisor_ip'],
+            cmd_load_rebal_interval)
+
         # inject traffic and wait to see if rebalance takes place
         rebalance, cpu_under_threshold = self.\
             autobalance_functionality(servers_dict,
                                       ABActionsEnum.AllCoresOverThreshold)
 
+        cmd_load_improvement_threshold = 'sudo ovs-vsctl --no-wait ' \
+            'set open_vSwitch . other_config:pmd-auto-lb-improvement' \
+            '-threshold="25"'
+        cmd_load_rebal_interval = 'sudo ovs-vsctl --no-wait set ' \
+            'open_vSwitch . other_config:pmd-auto-lb-rebal-interval=' \
+            '"2"'
+
+        LOG.info('Reset load_threshold cmd {}'.
+                 format(cmd_load_improvement_threshold))
+        shell_utils.run_command_over_ssh(
+            servers_dict['testpmd']['hypervisor_ip'],
+            cmd_load_improvement_threshold)
+        LOG.info('Reset load_threshold cmd {}'.
+                 format(cmd_load_rebal_interval))
+        shell_utils.run_command_over_ssh(
+            servers_dict['testpmd']['hypervisor_ip'],
+            cmd_load_rebal_interval)
         # evaluate if the testcase passed/failed
         msg = 'Unexpected rebalance took place, rebalance {}, ' \
               'cpu_under_threshold {}'.format(rebalance,
@@ -303,7 +338,7 @@ class TestMultiqueueScenarios(base_test.BaseTest):
                                            load_threshold * 0.5)
         elif action == ABActionsEnum.AllCoresOverThreshold:
             pps = multiqueue.load_all_cores(pmd_cores,
-                                            max(load_threshold * 1.5, 80))
+                                            load_threshold)
         elif action == ABActionsEnum.AllCoresBelowThreshold:
             pps = multiqueue.load_all_cores(pmd_cores,
                                             load_threshold * 0.5)
@@ -313,7 +348,7 @@ class TestMultiqueueScenarios(base_test.BaseTest):
         # to check rebalance 2 times as in the first time it may be possible
         # that it is checked just when we have started traffic and the load
         # is not over the threshold yet.
-        timeout = int(interval * 2.20)
+        timeout = int(interval * 1.50)
 
         # create injection command and start injection
         inj_cmd = "/opt/trex/current/multiqueue.py  --action gen_traffic " \
