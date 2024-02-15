38018d33a5ab3f289a4281b33ec20ebcb7d63c25
Miguel Angel Nieto Jimenez <mnietoji@redhat.com>
2024-02-15 14:43:24 +0100

    osp18
    Change-Id: Id3499e4e4c476212a943ba86014c6a32966a525d

--- nfv_tempest_plugin/services/os_clients.py
+++ nfv_tempest_plugin/services/os_clients.py
@@ -1,12 +1,10 @@
 from nfv_tempest_plugin.services.heat_client import HeatClient
-from nfv_tempest_plugin.services.metalsmith_client import MetalsmithClient
 from nfv_tempest_plugin.services.nova_client import NovaClient
 from nfv_tempest_plugin.services.swift_client import SwiftClient
 
 
-class OsClients(HeatClient, NovaClient, MetalsmithClient, SwiftClient):
+class OsClients(HeatClient, NovaClient, SwiftClient):
     def __init__(self):
         super().set_heat_clients()
         super().set_nova_clients()
-        super().set_metalsmith_clients()
         super().set_swift_clients()
