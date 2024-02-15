89dae15099ae35a3a135d2d8c39d39225f06ccd9
Miguel Angel Nieto Jimenez <mnietoji@redhat.com>
2024-02-15 14:43:24 +0100

    OSP18 pluging adaptation
    It is needed to do several modifications in order to run the plugin in
    osp18:
    * osp18 code will be in a new brach in which it will be
      removed dependencies to osp16/17
    * overcloud key in a different path
    * Remove undercloud clients, no undercloud in osp18
    * No metalsmith in osp18
    * host aggregation done in a different way
      https://issues.redhat.com/browse/OSPRH-5197
    * Different paths for some configuration files:
      /var/lib/openstack/config/nova/04-cpu-pinning-nova.conf
      /var/lib/openstack/config/nova/01-nova.conf
      /var/lib/openstack/config/nova/03-sriov-nova.conf
    * Get hypervisors from APIs
    * Remove dependencies on hypervisor name (compute-0, compute-1)
    * Configure plugin requirements
      pip install -chttps://releases.openstack.org/constraints/upper/2023.1 ./nfv-tempest-plugin
    * Remove instack.json file dependency that is not present in osp18
    
    Change-Id: Id3499e4e4c476212a943ba86014c6a32966a525d

--- nfv_tempest_plugin/services/keystone_client.py
+++ nfv_tempest_plugin/services/keystone_client.py
@@ -1,8 +1,5 @@
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
-from nfv_tempest_plugin.tests.common.collect_info \
-    import CollectInfo
-import openstack
 from oslo_log import log as logging
 from tempest import config
 
@@ -14,24 +11,6 @@ LOG = logging.getLogger('{} [-] nfv_plugin_test'.format(__name__))
 class KeystoneClient():
     @classmethod
     def set_keystone_clients(cls):
-        cls.uc_server_client = CollectInfo.check_client()
-
-        if cls.uc_server_client == 'nova':
-            undercloud_rc = cls.parse_rc_file(
-                CONF.nfv_plugin_options.undercloud_rc_file)
-
-            cls.undercloud_keystone_session = session.Session(auth=v3.Password(
-                auth_url=undercloud_rc['OS_AUTH_URL'],
-                username=undercloud_rc['OS_USERNAME'],
-                password=undercloud_rc['OS_PASSWORD'],
-                project_name=undercloud_rc['OS_PROJECT_NAME'],
-                user_domain_name=undercloud_rc['OS_USER_DOMAIN_NAME'],
-                project_domain_name=undercloud_rc[
-                    'OS_PROJECT_DOMAIN_NAME']), verify=False)
-        else:
-            cls.undercloud_keystone_session = openstack.connect(
-                cloud='undercloud', verify=False)
-
         cls.overcloud_keystone_session = session.Session(auth=v3.Password(
             auth_url=CONF.identity.uri,
             username=CONF.auth.admin_username,
@@ -39,27 +18,3 @@ class KeystoneClient():
             project_name=CONF.auth.admin_project_name,
             user_domain_name=CONF.auth.admin_domain_name,
             project_domain_name=CONF.auth.admin_domain_name), verify=False)
-
-    @classmethod
-    def parse_rc_file(cls, rc_file):
-        """Parse rc file
-
-        Parse rc file and take OSP related variables
-
-        :param rc_file: path to the rc file
-        :type rc_file: string
-        """
-        config = {}
-        try:
-            with open(rc_file, 'r') as rc:
-                for line in rc.read().split('\n'):
-                    if 'export OS_' in line:
-                        param = line.split('=')
-                        config[param[0].replace('export ', '')] = \
-                            param[1].replace('\'', '')
-        except Exception as err:
-            LOG.info('The following exceptions occurred while trying '
-                     'to parse rc file {}'.format(err))
-            raise err
-
-        return config
